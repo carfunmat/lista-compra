1d14a092c008d2d0a4936afd5cf44b12cc478c35
Carlos Funalleras <cfmcarlos@hotmail.com>
2022-11-28 23:48:40 +0100

    Commit inicial

---
+++ src/main/java/es/carlosfunalleras/listacompra/ListaCompraApplication.java
@@ -0,0 +1,13 @@
+package es.carlosfunalleras.listacompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaCompraApplication.class, args);
+	}
+
+}
